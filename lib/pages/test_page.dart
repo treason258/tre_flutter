16df22081942d5f9aca92b367f20d2c7f1e1afe7
matengfei <matengfei@xin.com>
2020-03-18 17:32:25 +0800

    整理TestPage

--- lib/pages/test_page.dart
+++ lib/pages/test_page.dart
@@ -5,6 +5,7 @@ import 'package:tre_flutter/pages/test/test_page_2.dart';
 import 'package:tre_flutter/pages/test/test_page_3.dart';
 import 'package:tre_flutter/pages/test/test_page_4.dart';
 import 'package:tre_flutter/pages/test/test_page_5.dart';
+import 'package:tre_flutter/pages/test/test_page_99.dart';
 import 'package:tre_flutter/utils/navigator_utils.dart';
 
 class TestPage extends StatefulWidget {
@@ -21,24 +22,42 @@ class _TestPageState extends State<TestPage> {
       appBar: AppBar(
         title: Text('TestPage'),
       ),
-      body: Container(
-        width: double.infinity,
-        height: double.infinity,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: <Widget>[
-            RaisedButton(
-              child: Text("TestPage0"),
-              onPressed: () {
-                NavigatorUtils.jumpToNewPage(context, "TestPage0", "Welcome to TestPage0");
-              },
-            ),
-            RaisedButton(child: Text("TestPage1"), onPressed: () => NavigatorUtils.jumpByWidget(context, TestPage1())),
-            RaisedButton(child: Text("TestPage2"), onPressed: () => NavigatorUtils.jumpByWidget(context, TestPage2())),
-            RaisedButton(child: Text("TestPage3"), onPressed: () => NavigatorUtils.jumpByWidget(context, TestPage3())),
-            RaisedButton(child: Text("TestPage4"), onPressed: () => NavigatorUtils.jumpByWidget(context, TestPage4())),
-            RaisedButton(child: Text("TestPage5"), onPressed: () => NavigatorUtils.jumpByWidget(context, TestPage5())),
-          ],
+      body: Scrollbar(
+        child: SingleChildScrollView(
+          padding: EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            //动态创建一个List<Widget>
+            children: <Widget>[
+              Row(children: <Widget>[
+                Expanded(
+                  child: RaisedButton(
+                    child: Text("---------------- NewPage ----------------"),
+                    onPressed: () {
+                      NavigatorUtils.jumpToNewPage(context, "NewPage", "Welcome to NewPage");
+                    },
+                  ),
+                ),
+              ]),
+              Row(children: <Widget>[Expanded(child: RaisedButton(child: Text("TestPage1-自带计数器示例"), onPressed: () => NavigatorUtils.jumpByWidget(context, TestPage1())))]),
+              Row(children: <Widget>[Expanded(child: RaisedButton(child: Text("TestPage2-展示时间"), onPressed: () => NavigatorUtils.jumpByWidget(context, TestPage2())))]),
+              Row(children: <Widget>[Expanded(child: RaisedButton(child: Text("TestPage3-列表"), onPressed: () => NavigatorUtils.jumpByWidget(context, TestPage3())))]),
+              Row(children: <Widget>[Expanded(child: RaisedButton(child: Text("TestPage4-轮播图"), onPressed: () => NavigatorUtils.jumpByWidget(context, TestPage4())))]),
+              Row(children: <Widget>[Expanded(child: RaisedButton(child: Text("TestPage5-闪屏页动画"), onPressed: () => NavigatorUtils.jumpByWidget(context, TestPage5())))]),
+              Row(children: <Widget>[Expanded(child: RaisedButton(child: Text("TestPage99"), onPressed: () => NavigatorUtils.jumpByWidget(context, TestPage99())))]),
+              Row(children: <Widget>[Expanded(child: RaisedButton(child: Text("TestPage99"), onPressed: () => NavigatorUtils.jumpByWidget(context, TestPage99())))]),
+              Row(children: <Widget>[Expanded(child: RaisedButton(child: Text("TestPage99"), onPressed: () => NavigatorUtils.jumpByWidget(context, TestPage99())))]),
+              Row(children: <Widget>[Expanded(child: RaisedButton(child: Text("TestPage99"), onPressed: () => NavigatorUtils.jumpByWidget(context, TestPage99())))]),
+              Row(children: <Widget>[Expanded(child: RaisedButton(child: Text("TestPage99"), onPressed: () => NavigatorUtils.jumpByWidget(context, TestPage99())))]),
+              Row(children: <Widget>[Expanded(child: RaisedButton(child: Text("TestPage99"), onPressed: () => NavigatorUtils.jumpByWidget(context, TestPage99())))]),
+              Row(children: <Widget>[Expanded(child: RaisedButton(child: Text("TestPage99"), onPressed: () => NavigatorUtils.jumpByWidget(context, TestPage99())))]),
+              Row(children: <Widget>[Expanded(child: RaisedButton(child: Text("TestPage99"), onPressed: () => NavigatorUtils.jumpByWidget(context, TestPage99())))]),
+              Row(children: <Widget>[Expanded(child: RaisedButton(child: Text("TestPage99"), onPressed: () => NavigatorUtils.jumpByWidget(context, TestPage99())))]),
+              Row(children: <Widget>[Expanded(child: RaisedButton(child: Text("TestPage99"), onPressed: () => NavigatorUtils.jumpByWidget(context, TestPage99())))]),
+              Row(children: <Widget>[Expanded(child: RaisedButton(child: Text("TestPage99"), onPressed: () => NavigatorUtils.jumpByWidget(context, TestPage99())))]),
+              Row(children: <Widget>[Expanded(child: RaisedButton(child: Text("TestPage99"), onPressed: () => NavigatorUtils.jumpByWidget(context, TestPage99())))]),
+            ],
+          ),
         ),
       ),
     );
