346e27c68b0a4b3a9278923b9bb5826296969731
matengfei <matengfei@xin.com>
2020-03-25 22:41:00 +0800

    新增ColorUtils和IconUtils

---
+++ lib/utils/color_utils.dart
@@ -0,0 +1,28 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+class ColorUtils {
+  static const Color transparent = Colors.transparent; // 透明
+  static const Color black = Colors.black; // 黑
+  static const Color white = Colors.white; // 白
+  static const Color grey = Colors.grey; // 灰
+
+  static const Color red = Colors.red; // 红
+  static const Color green = Colors.green; // 绿
+  static const Color yellow = Colors.yellow; // 黄
+  static const Color blue = Colors.blue; // 蓝
+
+  static const Color amber = Colors.amber; // 黄褐色
+  static const Color brown = Colors.brown; // 棕色色
+  static const Color cyan = Colors.cyan; // 蓝绿色
+  static const Color indigo = Colors.indigo; // 靛蓝色
+  static const Color lime = Colors.lime; // 绿黄色
+  static const Color orange = Colors.orange; // 橙色
+  static const Color pink = Colors.pink; // 粉红
+  static const Color purple = Colors.purple; // 紫色
+  static const Color teal = Colors.teal; // 鸭绿色
+
+  static const Color ffeeeeee = Color(0xffeeeeee);
+  static const Color ff888888 = Color(0xff888888);
+}
---
+++ lib/utils/icon_utils.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class IconUtils {
+  static const IconData home = Icons.home; // 首页
+  static const IconData person = Icons.person; // 我的
+  static const IconData error = Icons.error; // 错误
+  static const IconData warning = Icons.warning; // 警告
+  static const IconData list = Icons.format_list_bulleted; // 列表
+}
